0c57b21b529dac894c19122c84892ae28c472c20
zeidlitz <zeidlitzerik@gmail.com>
2024-12-03 06:58:57 +0100

    day1

--- day1/main.py
+++ day1/main.py
@@ -20,11 +20,8 @@ for number in range(len(left_list)):
     right_number = right_list[number]
     distances.append(abs(left_number - right_number))
 
-print("Part 1")
-print("distances        : ", sum(distances))
-
 for number in left_list:
     similarity_list.append(number * right_list.count(number))
 
-print("Part 2")
+print("distances        : ", sum(distances))
 print("similarity score : ", sum(similarity_list))
