d68df34be1ce0331de0024574ebe89452e6a60f2
zeidlitz <zeidlitzerik@gmail.com>
2024-12-08 12:07:28 +0100

    day8

--- day8/main.py
+++ day8/main.py
@@ -16,7 +16,7 @@ def is_within_bounds(r, c, rows, columns):
     return True
 
 
-def place_signal_on_antenna_map(r0, c0, r1, c1, dr, dc, rows, columns, result):
+def place_signal(r0, c0, r1, c1, dr, dc, rows, columns, result):
     north_r = -1 * dr
     north_c = -1 * dc
     south_r = dr
@@ -31,7 +31,7 @@ def place_signal_on_antenna_map(r0, c0, r1, c1, dr, dc, rows, columns, result):
         result.append((south_signal_r, south_signal_c))
 
 
-def find_row_column_differences(grid):
+def find_signals(grid):
     result = []
     from collections import defaultdict
     positions = defaultdict(list)
@@ -52,39 +52,73 @@ def find_row_column_differences(grid):
                 c1 = pos_list[j][1]
                 row_diff = r1 - r0
                 col_diff = c1 - c0
-                place_signal_on_antenna_map(r0, c0, r1, c1, row_diff, col_diff, rows, columns, result)
-
+                place_signal(r0, c0, r1, c1, row_diff, col_diff, rows, columns, result)
     return result
+
+
+def place_harmonic(r0, c0, r1, c1, dr, dc, rows, columns, result):
+    north_r = -1 * dr
+    north_c = -1 * dc
+    south_r = dr
+    south_c = dc
+    north_signal_r = r0 + north_r
+    north_signal_c = c0 + north_c
+    south_signal_r = r1 + south_r
+    south_signal_c = c1 + south_c
+
+    while (is_within_bounds(north_signal_r, north_signal_c, rows, columns)):
+        result.append((north_signal_r, north_signal_c))
+        north_signal_r += + north_r
+        north_signal_c += + north_c
+    while (is_within_bounds(south_signal_r, south_signal_c, rows, columns)):
+        result.append((south_signal_r, south_signal_c))
+        south_signal_r += south_r
+        south_signal_c += south_c
+
+
+def find_harmonics(grid):
+    result = []
+    antennas = []
+    from collections import defaultdict
+    positions = defaultdict(list)
+    rows = len(grid)
+    columns = len(grid[0])
+    for row in range(rows):
+        for col in range(columns):
+            element = grid[row][col]
+            if element != ".":
+                antennas.append((row, col))
+                positions[element].append((row, col))
+
+    for element, pos_list in positions.items():
+        for i in range(len(pos_list)):
+            for j in range(i + 1, len(pos_list)):
+                r0 = pos_list[i][0]
+                c0 = pos_list[i][1]
+                r1 = pos_list[j][0]
+                c1 = pos_list[j][1]
+                row_diff = r1 - r0
+                col_diff = c1 - c0
+                place_harmonic(r0, c0, r1, c1, row_diff, col_diff, rows, columns, result)
+    return antennas, result
 
 
 def part_one(input_data):
     antenna_grid = input_data
-    result = find_row_column_differences(antenna_grid)
+    result = find_signals(antenna_grid)
     result = list(set(result))
-    for s in result:
-        print(s)
     return len(result)
 
 
 def part_two(input_data):
-    return 0
+    antenna_grid = input_data
+    antennas, result = find_harmonics(antenna_grid)
+    result = [item for item in result if item not in antennas]
+    result = list(set(result))
+    return len(antennas) + len(result)
 
 
 if __name__ == "__main__":
     input_data = parse_input()
     print(f"Part 1 = {part_one(input_data)}")
     print(f"Part 2 = {part_two(input_data)}")
-
-
-"""
-1. for every antenna, find other similar antennas
-2. what is the row/column distance between them. For example a[1][3] and a[2][4] have a distance of [1][1], namely one should walk up and to the right to find the frequency.
-3. Check if the frequency is within the bounds of the map
-4. If within the bounds, mark the frequency on the frequency map
-
-uncertainties:
-- two different frequencies can occour at the same place?
-- we should probably not count frequencies of same type in same place?
-
-first try: 312 too high
-"""
