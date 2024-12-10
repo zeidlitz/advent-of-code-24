0cf394fd6b9dc00dd9ac7c1ae178de25846ad906
Erik <erik.zeidlitz@sudosweden.com>
2024-12-10 12:48:03 +0100

    day2

--- day2/main.py
+++ day2/main.py
@@ -1,11 +1,12 @@
-with open("input.txt", "r") as file:
-    lines = file.readlines()
-
-reports = []
-for line in lines:
-    a = line.strip().split()
-    a = [int(item) for item in a]
-    reports.append(a)
+def parse_input(input_data="input.txt"):
+    with open(input_data, 'r') as file:
+        lines = file.readlines()
+    reports = []
+    for line in lines:
+        a = line.strip().split()
+        a = [int(item) for item in a]
+        reports.append(a)
+    return reports
 
 
 def is_safe(report):
@@ -34,8 +35,35 @@ def is_safe_with_dampener(report):
     return False
 
 
-safe_reports = [report for report in reports if is_safe(report)]
-safe_reports_with_dampener = [report for report in reports if is_safe_with_dampener(report)]
+def part_one(input_data):
+    reports = input_data
+    safe_reports = [report for report in reports if is_safe(report)]
+    return len(safe_reports)
 
-print(f"Number of safe reports              : {len(safe_reports)}")
-print(f"Number of safe reports with dampener: {len(safe_reports_with_dampener)}")
+
+def part_two(input_data):
+    reports = input_data
+    safe_reports_with_dampener = [report for report in reports if is_safe_with_dampener(report)]
+    return len(safe_reports_with_dampener)
+
+
+def test_solution(func, input_data, result):
+    GREEN = "\033[92m"
+    RED = "\033[91m"
+    RESET = "\033[0m"
+    example_input_data = parse_input(input_data)
+    expected_output = result
+    actual_output = func(example_input_data)
+
+    if actual_output == expected_output:
+        print(f"Test for {func.__name__} - {GREEN}PASSED{RESET}: expected {actual_output}, got {actual_output}")
+    else:
+        print(f"Test for {func.__name__} - {RED}FAILED{RESET}: expected {expected_output}, got {actual_output}")
+
+
+if __name__ == "__main__":
+    input_data = parse_input()
+    test_solution(part_one, "example.txt", 2)
+    print(f"Part 1 = {part_one(input_data)}")
+    test_solution(part_two, "example.txt", 4)
+    print(f"Part 2 = {part_two(input_data)}")
